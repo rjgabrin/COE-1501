9316a730d026951ae7d03b2366250e914b5de488
Ryan Gabrin <36611105+rjgabrin@users.noreply.github.com>
2018-10-24 17:12:50 -0400

    Initial commit

---
+++ Project 3/LZWmod.java
@@ -0,0 +1,20 @@
+//Ryan Gabrin
+//Peoplesoft: 4101706
+//Email: rjg69@pitt.edu
+//Khattab Tu/Th 2:30-3:45
+//1501 Fall 2018 - Assignment 3
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class LZWmod{
+
+    public static void main(String[] args){
+
+
+        System.out.println("Goodbye.");
+        System.exit(0);
+    }
+
+}
